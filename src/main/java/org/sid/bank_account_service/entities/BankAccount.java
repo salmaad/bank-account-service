ccaa04b3bb11605e441004a69563fcb8f51dc51d
salmaad <salma.ayad.02@gmail.com>
2025-10-09 12:15:24 +0100

    first micro service REST

---
+++ src/main/java/org/sid/bank_account_service/entities/BankAccount.java
@@ -0,0 +1,24 @@
+package org.sid.bank_account_service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.bank_account_service.enums.AccountType;
+
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class BankAccount {
+    @Id
+    private String id;
+    private Date createdAt;
+    private double balance;
+    private String currency;
+    private AccountType type;
+}
