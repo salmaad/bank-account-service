962dfa8461dcf6f9e1936d9e77500e0688b0632b
salmaad <salma.ayad.02@gmail.com>
2025-10-16 00:57:17 +0100

    Swagger and Spring Data Rest

--- src/main/java/org/sid/bank_account_service/repositories/BankAccountRepository.java
+++ src/main/java/org/sid/bank_account_service/repositories/BankAccountRepository.java
@@ -2,7 +2,9 @@ package org.sid.bank_account_service.repositories;
 
 import org.sid.bank_account_service.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
     String id(String id);
 }
