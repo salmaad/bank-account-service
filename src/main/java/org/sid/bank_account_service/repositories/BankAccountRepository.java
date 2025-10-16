9ae48a1bec0be149dfb29b3968fd837490a6b4c6
salmaad <salma.ayad.02@gmail.com>
2025-10-16 01:09:00 +0100

    Projection

--- src/main/java/org/sid/bank_account_service/repositories/BankAccountRepository.java
+++ src/main/java/org/sid/bank_account_service/repositories/BankAccountRepository.java
@@ -1,10 +1,13 @@
 package org.sid.bank_account_service.repositories;
 
 import org.sid.bank_account_service.entities.BankAccount;
+import org.sid.bank_account_service.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
-    String id(String id);
+    List<BankAccount> findByType (AccountType type);
 }
